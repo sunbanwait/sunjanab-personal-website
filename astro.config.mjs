7716edf34ff03818f489aa1a839f03f9e5d08503
sbanwait-wq <sbanwait@scu.edu>
2026-09-03 09:44:08 -0700

    Add Vercel analytics integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import Analytics from '@vercel/analytics/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), Analytics()],
   vite: {
     plugins: [tailwindcss()],
   },
